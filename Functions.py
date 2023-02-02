86cd00ead66a8850ae523f2a3e7bfab1b5d3e00e
VincentLepeytre <123426279+VincentLepeytre@users.noreply.github.com>
2023-02-02 12:24:02 -0500

    Add files via upload

--- Functions.py
+++ Functions.py
@@ -6,13 +6,16 @@ import matplotlib.pyplot as plt
 from scipy import optimize
 import scipy.optimize as sco
 
-
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.linear_model import Lasso,LassoCV
+from sklearn.metrics import mean_squared_error,r2_score,mean_absolute_percentage_error
+from sklearn.preprocessing import StandardScaler
 
 #################### Importer les données ####################
 
 
 # Loader Industry data (Veuillez ajouter votre path)
-def Load_ind_data(path = "C:/Users/Sébastien/Desktop/TP1/48_Industry_Portfolios.CSV"):
+def Load_ind_data(path):
     
     dateparse = lambda x: pd.datetime.strptime(x,'%Y%m') 
 
@@ -26,7 +29,7 @@ def Load_ind_data(path = "C:/Users/Sébastien/Desktop/TP1/48_Industry_Portfolios
 
 
 #Loader RF data (À voir si on change la méthodologie pour la sélection)
-def Load_rf(path = "C:/Users/Sébastien/Desktop/TP1/SOFR30DAYAVG.xls", date = '2021-08-01'):
+def Load_rf(path, date = '2022-08-01'):
     
     Data_SOFR = pd.read_excel(path)
 
@@ -37,12 +40,23 @@ def Load_rf(path = "C:/Users/Sébastien/Desktop/TP1/SOFR30DAYAVG.xls", date = '2
 
 
 
-    Expected_Risk_free = Data_SOFR[Data_SOFR.index >= date].mean() # do again 
+    #Expected_Risk_free = Data_SOFR[Data_SOFR.index >= date].mean() # do again 
     
-    return  Expected_Risk_free.iloc[0]
+    # Keep the last value : 
+    
+    Expected_Risk_free = Data_SOFR.iloc[-1]
+    # convert the annual riskfree rate to per period
+    rf_per_period = ((1+Expected_Risk_free)**(1/12))-1
+    
+    return  (rf_per_period.iloc[0])
 
+# Load factor and macroeconomics  data for the prediction of returns
 
-
+def Load_lasso_variable(path_regression):
+    Variable_regression = pd.read_excel(path_regression ,header=0,skiprows=3)
+    Variable_regression['observation_date'] = pd.to_datetime(Variable_regression['observation_date'], format='%Y-%m-%d')
+    Variable_regression.set_index('observation_date', inplace=True)
+    return(Variable_regression)
 
 
 #################### Without risk-free asset : ####################
@@ -259,20 +273,27 @@ def Ptf_target_optimization_W_Rf(E_return, E_cov, Expected_Risk_free, Nbr_PTF, b
 #################### Porfolio Tangent & GMV Functions ####################
 
 
-def annualize_rets(r, periods_per_year):
-    """
-    Annualizes a set of returns
-    """
-    compounded_growth = (1+r).prod()
-    n_periods = r.shape[0]
-    return compounded_growth**(periods_per_year/n_periods)-1
+#def annualize_rets(r, periods_per_year):
+#    """
+#    Annualizes a set of returns
+#    """
+#    compounded_growth = (1+r).prod()
+#    n_periods = r.shape[0]
+#    return compounded_growth**(periods_per_year/n_periods)-1
+
+#def annualize_rets_prediction(r, periods_per_year):
+#    """
+#    Annualizes a set of returns
+#    """
+#    R_A = (((1+r)**periods_per_year)-1)
+#    return R_A
 
 
-def annualize_vol(r, periods_per_year):
-    """
-    Annualizes the vol of a set of returns
-    """
-    return r.std()*(periods_per_year**0.5)
+#def annualize_vol(r, periods_per_year):
+#    """
+#    Annualizes the vol of a set of returns
+#    """
+#    return r.std()*(periods_per_year**0.5)
 
 
 def sharpe_ratio(r, riskfree_rate, periods_per_year):
@@ -280,11 +301,13 @@ def sharpe_ratio(r, riskfree_rate, periods_per_year):
     Computes the annualized sharpe ratio of a set of returns
     """
     # convert the annual riskfree rate to per period
-    rf_per_period = (1+riskfree_rate)**(1/periods_per_year)-1
+    #rf_per_period = (1+riskfree_rate)**(1/periods_per_year)-1
     excess_ret = r - rf_per_period
-    ann_ex_ret = annualize_rets(excess_ret, periods_per_year)
-    ann_vol = annualize_vol(r, periods_per_year)
-    return ann_ex_ret/ann_vol
+    
+    #ann_ex_ret = annualize_rets(excess_ret, periods_per_year)
+    #ann_vol = annualize_vol(r, periods_per_year)
+    vol =r.std()
+    return ex_ret.mean()/vol
 
 
 
@@ -349,11 +372,221 @@ def msr(risk_free_rate, er, cov, bounds):
                        constraints=(weights_sum_to_1),
                        bounds = bounds
                        )
-    return results
+    return results.x
+
+#################### Determination of the expected return  : ####################
+
+##### Lasso regression  #####
+
+def Split_dataset(Data_return,Variable_regression,Date_variable,Date_train,Date_test,Last_date):
+    
+    
+    # Gestion Date :  
+    Data_return=Data_return.append(Data_return.iloc[-1])
+    Date = Data_return.index.values
+    Date[-1]='2022-12-01T00:00:00.000000000'
+    Data_return.index=Date
+
+# Split the dataset of variable : 
+
+    V_R_train = Variable_regression[Variable_regression.index  <= Date_train]
+    V_R_test = Variable_regression[Variable_regression.index >= Date_test]
+    V_R_test = V_R_test[V_R_test.index <= Last_date]
+    
+    # Construction of the lagged return :
+    
+    Return_L1 = Data_return.shift(periods=1).iloc[1:,:]
+    
+    Return_l1_train = Return_L1[Return_L1.index >= Date_variable]
+    Return_l1_train = Return_l1_train[Return_l1_train.index <= Date_train]
+    
+    Return_l1_test =  Return_L1[Return_L1.index >= Date_test]
+    Return_l1_test =  Return_l1_test[Return_l1_test.index <= Last_date]
+    
+    #
+    
+    Return_L2 = Data_return.shift(periods=2).iloc[2:,:]
+    
+    Return_l2_train = Return_L2[Return_L2.index >= Date_variable]
+    Return_l2_train = Return_l2_train[Return_l2_train.index <= Date_train]
+    
+    Return_l2_test =  Return_L2[Return_L2.index >= Date_test]
+    Return_l2_test =  Return_l2_test[Return_l2_test.index <= Last_date]
+    
+    #
+    
+    Return_L3 = Data_return.shift(periods=3).iloc[3:,:]
+    
+    Return_l3_train = Return_L3[Return_L3.index >= Date_variable]
+    Return_l3_train = Return_l3_train[Return_l3_train.index <= Date_train]
+    
+    Return_l3_test =  Return_L3[Return_L3.index >= Date_test]
+    Return_l3_test =  Return_l3_test[Return_l3_test.index <= Last_date]
+    
+    Return_L3 = Data_return.shift(periods=3).iloc[3:,:]
+# Traitement of the dataset : 
+
+    V_R_train=V_R_train.reset_index()
+    V_R_train=V_R_train.drop(columns=["observation_date"])
+
+    V_R_test=V_R_test.reset_index()
+    V_R_test=V_R_test.drop(columns=["observation_date"])
+    
+ 
+    Return_l1_train=Return_l1_train.reset_index()
+    Return_l1_train=(Return_l1_train.drop(columns=["index"]))
+    
+    
+    Return_l1_test=Return_l1_test.reset_index()
+    Return_l1_test=(Return_l1_test.drop(columns=["index"]))
+    
+    Return_l2_train=Return_l2_train.reset_index()
+    Return_l2_train=(Return_l2_train.drop(columns=["index"]))
+    
+    
+    Return_l2_test=Return_l2_test.reset_index()
+    Return_l2_test=(Return_l2_test.drop(columns=["index"]))
+    
+    
+    Return_l3_train=Return_l3_train.reset_index()
+    Return_l3_train=(Return_l3_train.drop(columns=["index"]))
+    
+    
+    Return_l3_test=Return_l3_test.reset_index()
+    Return_l3_test=(Return_l3_test.drop(columns=["index"]))
+    
+# Split the dataset of output :  
+
+    Y= Data_return[Data_return.index >= Date_variable]
+    Y_train = Y[Y.index <= Date_train]
+    Y_test = Y[Y.index >= Date_test]
+    Y_test = Y_test[Y_test.index < Last_date]
+    
+# Traitement of the dataset : 
+
+    Y_train=Y_train.reset_index()
+    Y_train=(Y_train.drop(columns=["index"]))/100
+
+    Y_test=Y_test.reset_index()
+    Y_test=(Y_test.drop(columns=["index"]))/100
+    
+
+    Data_regression_lasso = dict([
+        ('V_R_train',V_R_train),
+        ('V_R_test',V_R_test),
+        ('Y_train',Y_train),
+        ('Y_test',Y_test),
+        ('Return_l1_train',Return_l1_train),
+        ('Return_l1_test',Return_l1_test),
+        ('Return_l2_train',Return_l2_train),
+        ('Return_l2_test',Return_l2_test),
+        ('Return_l3_train',Return_l3_train),
+        ('Return_l3_test',Return_l3_test)
+    ])
+    
+    return(Data_regression_lasso)
 
 
+def Lasso_regression(Data_regression_lasso):
+    
+# Input lasso regression : 
+
+    
+    Y_train = Data_regression_lasso ['Y_train']
+    Y_test = Data_regression_lasso ['Y_test']
+    
+   
+    result_lasso = np.zeros((6,len(Y_train.columns)))
+    
 
 
+# Regression for each industry : 
+
+    for i in range (0,len(Y_train.columns)):
+        
+        
+        V_R_train = Data_regression_lasso ['V_R_train']
+        V_R_test = Data_regression_lasso ['V_R_test']
+        
+        industrie = Y_train.columns[i]
+        
+        
+        V_R_train["Return_l1"] = Data_regression_lasso ['Return_l1_train'][industrie]
+        V_R_test["Return_l1"]=  Data_regression_lasso ['Return_l1_test'][industrie]
+        
+        V_R_train["Return_l2"] = Data_regression_lasso ['Return_l2_train'][industrie]
+        V_R_test["Return_l2"]=  Data_regression_lasso ['Return_l2_test'][industrie]
+        
+        V_R_train["Return_l3"] = Data_regression_lasso ['Return_l3_train'][industrie]
+        V_R_test["Return_l3"]=  Data_regression_lasso ['Return_l3_test'][industrie]
+        
+        # Standardisation : 
+
+        scaler = StandardScaler().fit(V_R_train) 
+        V_R_train = scaler.transform(V_R_train)
+        V_R_test = scaler.transform(V_R_test)
+    
+        
+    
+    # Model parametrization :
+        tscv = TimeSeriesSplit(n_splits=2,test_size=8) # Cross validatation 
+        model = LassoCV(cv=tscv) # try to find the optimal alpha (penalization)
+        model.fit(V_R_train, Y_train[industrie])
+    
+    # Fit the model to the return of the specific industry 
+    
+        lasso_best = Lasso(alpha=model.alpha_)
+        lasso_best.fit(V_R_train, Y_train[industrie])
+    
+    # Prediction : 
+    
+        pred = lasso_best.predict(V_R_test)
+    
+    # keep only the last prediction for next month 
+        Last_pred = pred[-1]
+    
+        R_squared_train = lasso_best.score(V_R_train, Y_train[industrie])*100
+        R_squared_test =lasso_best.score(V_R_test[0:-1], Y_test[industrie])*100
+        
+        #  Performance measurement out of sample:
+        
+        MSE_test_set = mean_squared_error(Y_test[industrie], pred[0:(len(pred)-1)])
+        
+        
+        mean_historique = np.array([Y_train[industrie].mean()] * len(Y_test[industrie]))
+        
+        MSE_test_set_mean = mean_squared_error(Y_test[industrie], mean_historique)
+        
+        ratio_mse = MSE_test_set/MSE_test_set_mean
+        
+        
+        
+        
+        result_lasso [:,i] = [Last_pred,R_squared_train,MSE_test_set,R_squared_test,MSE_test_set_mean,ratio_mse]
+        
+    Resultat_prediction = pd.DataFrame(result_lasso, columns=Y_train.columns, index = ['E_R','R_squared_train','MSE_test_set','R_squared_test','MSE_test_set_mean','ratio_mse'])
+    
+    return(Resultat_prediction)
+
+def select_pred(Resultat_prediction,Data) :
+    """ Select only the prediction where the lasso approch perform better on the train set base on MSE ratio and R square
+    """
+    R_T = Resultat_prediction.T
+    
+    # Selection criterion : MSE_ratio < 1 and/or R_squared > 0 on train set :
+    
+    R_T['E_R'] = np.where((R_T['R_squared_test'] <= 0) | (R_T['ratio_mse'] >= 1), np.NaN, R_T['E_R'])
+    R_T = R_T.dropna()
+    
+    # For each industrie where the prediction base on lasso are not keep we use the historical mean :
+    
+    E_return_mean =Data.mean()/100
+    E_return_mean=E_return_mean.drop(index=R_T.index)
+    E_return_mean.name = 'E_R'
+    
+    E_return_select = R_T['E_R'].append(E_return_mean)
+    
+    return(E_return_select)
 
 #################### Plotting Functions ####################
 
@@ -375,7 +608,7 @@ def plot_ef(E_return, E_cov, Expected_Risk_free, Nbr_PTF, bounds,  show_cml=Fals
     #Start by plotting result of efficient frontier without rf
     plt = Efficient_frontiere_result['Efficient_frontiere']['Returns'].plot(kind='line',figsize=(15,11), 
                                                                         xlim = [0, max(Efficient_frontiere_result['Efficient_frontiere'].index)+0.5], 
-                                                                        ylim = [0, max(Efficient_frontiere_result['Efficient_frontiere']['Returns'])+0.2]) 
+                                                                        ylim = [min(Efficient_frontiere_result['Efficient_frontiere']['Returns'])-0.3, max(Efficient_frontiere_result['Efficient_frontiere']['Returns'])+0.2]) 
     if show_cml :
         Efficient_frontiere_result_W_RF = Ptf_target_optimization_W_Rf(E_return, E_cov,
                                                                    Expected_Risk_free, Nbr_PTF,
